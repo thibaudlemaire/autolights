c55fc5918097ad59350092438b42ab4504ff312d
asalmona <antoine.salmona@telecom-paristech.fr>
2017-06-21 14:15:25 +0200

    low freq energie

---
+++ soft/sys_expert/low_freq.py
@@ -0,0 +1,46 @@
+from scipy.io import wavfile
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.io.wavfile as wav
+from scipy.signal import *
+
+
+filename='drop_3.wav'
+
+fe,signal=wavfile.read(filename)
+f=fe
+signal=signal[:,1]
+
+
+
+plt.specgram(signal, NFFT=2048, Fs=fe, noverlap=3*2048/4)
+plt.show()
+
+
+
+
+def calcul_energie(signal,a) :
+    ech=len(signal)
+    energie = np.zeros(ech)
+    energie[0]= signal[0]
+    n=0
+    for n in range (1, ech):
+        energie[n]=a*energie[n-1]+ (1.0-a)*(signal[n]**2)
+    return energie 
+
+energie= calcul_energie(signal, 0.99995)
+
+
+fe=fe*1.0/echantillonnage
+
+energie=energie/max(energie)
+energie =lfilter(np.hanning(100),1,energie)
+   
+##low pass filter
+    
+b3,a3 = iirfilter(N=3,Wn=[100.0/fe*2],btype="lowpass",ftype="butter")
+#freq de coupure fc/fe=0.1 si Wn= [0.1¨*2]
+
+plt.figure()
+plt.plot(np.arange(len(signal))*1.0/fe, calcul_energie(lfilter(b3,a3, signal), 0.99995))
+plt.show()
