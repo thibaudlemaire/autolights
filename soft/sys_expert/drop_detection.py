5973f7c75c672c6fd8822d1404946e3054cf9df0
Jane DOUAT <jane.douat@telecom-paristech.fr>
2017-06-21 11:25:54 +0200

    drop : choix des creux, passe-bas

--- soft/sys_expert/drop_detection.py
+++ soft/sys_expert/drop_detection.py
@@ -13,8 +13,11 @@ import librosa
 import scipy.fftpack as scfft
 from scipy.signal import *
 
+from scipy.io import wavfile
+  
+
     #OPEN FILE
-filename ='Khaer - Chapter 1-shorter.wav'
+filename ='Avicii drop.wav'
 
 fe, signal = wavfile.read(filename) 
 
@@ -43,8 +46,8 @@ def ech_function(sound, temps, ech) :
 
 
 
-plt.figure()             
-plt.plot(np.arange(len(signal))*1.0/fe, signal, 'r')
+#plt.figure()             
+#plt.plot(np.arange(len(signal))*1.0/fe, signal, 'r')
 #décale de 4ms l'échantillonnage 200
                
     #CALCUL DE L'ÉNERGIE
@@ -61,7 +64,7 @@ def calcul_energie(signal,a) :
 
 
 energie= calcul_energie(signal, 0.99995)
-echantillonnage =1800
+echantillonnage =100
 time, energie = ech_function(energie, time, echantillonnage)
 fe=fe*1.0/echantillonnage
 
@@ -70,177 +73,69 @@ energie =lfilter(np.hanning(100),1,energie)
 plt.figure()
 plt.plot(time, energie, 'g')
 
-"""
-    #dérivation
-N=4
-
-derivee = np.zeros(len(time))
-
-#le premier n pour lequel on calcule la dérivée est celui N points après le début
-for p in range (0,N): #va jusqu'à N-1
-    derivee[2*N]=derivee[2*N]+ energie[2*N-p] - energie[2*N-p-N]
-    
-    #on a d[2N]
-
-#on calcule ceux d'après par récurrence
-
-for n in range (2*N, len(energie)-1) :#faire minus one
-    derivee[n+1] = derivee[n] + energie[n-(2*N-1)] + energie[n+1] -2*energie[n-(N-1)]
-    #derivee[n+1] = derivee[n] + energie[n-(2*N-1)] + energie[n+1] -2*energie[n-(N-1)]
-    #derivee[n+1] = derivee[n] -energie[n-(N-1)]+ energie[n+1] + energie[n-N] - energie[n +1 -(2*N-1)] 
-
-plt.figure()    
-plt.plot(time, derivee, color = 'r')
-
-energie=derivee
-"""
-a=1
-derivee = np.diff(energie, a)
-plt.figure()
-plt.plot(time[a:], derivee[a:])
-
 
     #detection de creux
 den = lfilter([+1,-1], 1, energie)
 den = np.sign(den)
 dden = lfilter([+1,-1], 1, den)
 ind = np.nonzero(dden==2)
-ind=np.array(ind)
+ind=np.array(ind) -1
 time=np.array(time)
-plt.figure()
+
 plt.plot(time[ind], energie[ind], 'o')
 
-creux = np.nonzero(energie[ind]<- -10)
 
+#creux = np.nonzero(energie[ind]<- -10)
 
-
-drops =[]
-creux = energie[ind][0]
-creux_time = time[ind]
-i=5
-k=0
-test= True
-for i in range (len(creux)-4) :
-    k=1
-    for k in range (5) :
-        if (creux[i]>creux[i-k] or creux[i]>creux[i+k]) :
-            test = False
-    k=1
-    if (test) :
-        drops.append(ind[0][i])
-        print ("there is a drop at " +  str(ind[0][i]*1.0/fe))
-    test= True
-    
-
-drops_final=[]
-l=0
-for l in range (len(drops)) :
-    if (drops[l]-1.0*fe > 0 and drops[l]-1 <len(energie)) :
-        energie_before = sum(energie[int(drops[l]-1.0*fe):drops[l]-1])*1.0/(fe+1)
-        energie_after = sum(energie[drops[l]+1:int(drops[l]+1.0*fe)]*1.0/(fe+1))
-        if energie_before < energie_after * 0.7 :
-            drops_final.append(drops[l]*1.0/fe)  
-
-
-
-
-
-"""
-j=complex(0,1)
-
-
-def idealFilter(n,fe):
-    h=[0]*n
-    h[0]=0
-    for i in range(1,n):
-        h[i]=(fe/i*np.cos(fe*np.pi*i)-1/(np.pi*i**2)*np.sin(fe*np.pi*i))
-    return h
-
-
-
-realFilter=lfilter(idealFilter(13071,fe),1,np.hanning(13071))
-
-plt.plot(time, lfilter(realFilter, 1, energie))
-
-
-    # on découpe en frames
-
-def frame_cut(sound, frame_time) :
-    frame_length = int(frame_time*fe)
-    frames = []
-    i =0
-    while (i<len(signal)-10) :
-        frames.append(sound[i:i+frame_length])
-        i = i + int(frame_length/2)
-    return frames    
+def find_peak(energie, temps,f) :
+    den = lfilter([+1,-1], 1, energie)
+    den = np.sign(den)
+    dden = lfilter([+1,-1], 1, den)
+    ind = np.nonzero(dden==2)
+    ind=np.array(ind) -1
+    list_peaks = []
+    ech = temps*f
+    i = 0
+    for i in range (len(ind[0])) :
+        peak = True
+        k=0
+        if (i==0) :
+            peak=False
+        while (k<len(ind[0]) and ind[0][k]-ind[0][i]<=ech) :
+            if (abs(ind[0][i]-ind[0][k])<=ech) :
+                if (energie[ind[0][i]] > energie[ind[0][k]]) :
+                    peak = False
+            k=k+1
+        if (peak == True) :
+            list_peaks.append(ind[0][i]*1.0/fe)
+        peak = False
+    return list_peaks
+peaks = find_peak(energie, 2.0,fe)
         
-frames = frame_cut(signal,0.010)
-
-plt.figure()
-plt.plot(np.linspace(0,0.01,len(frames[-1])), frames[-1])
-
-    #CALCUL DE LA TFTD DE CE SIGNAL
-sound=frames[0]
-tftd = scfft.fft(sound)
-freq = scfft.fftfreq(len(sound),1.0/fe)
-
-tftdmel=[0]*int(1127*np.log(1+len(tftd)/700))
-for i in range (len(tftd)):
-    tftdmel[int(1127*np.log(1+i/700))]=tftd[i]
-
-
-tftd_mel=[0]*int(700*(np.exp(len(tftd)*1.0/1127)-1))
-for i in range (int(700*(np.exp(len(tftd)*1.0/1127)-1))) :
-    tftd_mel[i] =tftd[int(700*(np.exp(i*1.0/1127)-1))]
-
-plt.figure()
-plt.plot(freq, tftd)
-
-plt.figure()
-plt.plot(freq[0:len(tftd_mel)], tftd_mel)
-
-"""
-
-
-
-"""
-    #rééchantillonnage
-#on a à ce point fe/180 = 441hz pour fe = 44100 hz
-echantillonnage = 100
-time, derivee= ech_function(derivee, time, echantillonnage)
-fe=fe/echantillonnage
-plt.figure()
-plt.plot(time, derivee, color = 'b')
-"""
-
-
-"""
-    #dérivation
-N=10
-
-derivee = np.zeros(len(time))
-
-#le premier n pour lequel on calcule la dérivée est celui N points après le début
-for p in range (0,N): #va jusqu'à N-1
-    derivee[2*N]=derivee[2*N]+ energie[2*N-p] - energie[2*N-p-N]
+    ##low pass filter
     
-    #on a d[2N]
+b3,a3 = iirfilter(N=2,Wn=[100.0/(fe*echantillonnage)*2],btype="lowpass",ftype="butter")
+#freq de coupure fc/fe=0.1 si Wn= [0.1¨*2]
+plt.figure()
+plt.plot(np.arange(len(signal))*1.0/(fe*echantillonnage), calcul_energie(lfilter(b3,a3, signal), 0.99995))
 
-#on calcule ceux d'après par récurrence
-
-for n in range (2*N, len(energie)-1) :#faire minus one brooo
-    derivee[n+1] = derivee[n] + energie[n-(2*N-1)] + energie[n+1] -2*energie[n-(N-1)]
-    #derivee[n+1] = derivee[n] + energie[n-(2*N-1)] + energie[n+1] -2*energie[n-(N-1)]
-    #derivee[n+1] = derivee[n] -energie[n-(N-1)]+ energie[n+1] + energie[n-N] - energie[n +1 -(2*N-1)] 
-
-plt.figure()    
-plt.plot(time, derivee, color = 'r')
-
-energie=derivee
-"""
+w,h=scipy.signal.freqz(b3,a3,4096)
+plt.figure()
+plt.plot(w, abs(h))
 
 
-
+    ### filtre de dérivation
+     
+j=complex(0,1)    
+def idealFilter(M,b):
+     n=np.arange(-M+1,M)
+     n[M-1]=1
+     h=(-2*np.pi)*((np.sin(np.pi*n*b*2)/2)-np.pi*b*n*np.cos(np.pi*n*b*2))/((np.pi**2)*n**2)
+     h[M-1]=0
+     return h
+        
+plt.figure()
+plt.plot(time, lfilter(idealFilter(1000,0.2)*hamming(1999), 1, lfilter(idealFilter(1000,0.2)*hamming(1999), 1, energie)))
 
 
 
