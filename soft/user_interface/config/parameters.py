ac9fb362842acb77e14745efa0c4bcdb3a38052e
Thibaud Lemaire <tlemaire@enst.fr>
2017-06-26 12:47:08 +0200

    Exemple set

--- soft/user_interface/config/parameters.py
+++ soft/user_interface/config/parameters.py
@@ -5,12 +5,14 @@ BEAT_EVENT = 1
 BPM_CHANGE_EVENT = 2
 GENDER_CHANGE_EVENT = 3
 HARMONIC_CHANGE_EVENT = 4
+DROP_EVENT = 5
 
 EVENTS_PARAM = [
     (BEAT_EVENT, 'Beat'),
     (BPM_CHANGE_EVENT, 'BPM change'),
     (GENDER_CHANGE_EVENT, 'Gender change'),
     (HARMONIC_CHANGE_EVENT, 'Harmonic change'),
+    (DROP_EVENT, 'Drop'),
 ]
 
 # Continuous
@@ -28,12 +30,19 @@ CONTINUOUS_PARAM = [
 
 VOICES_BOOLEAN = 1
 GENDER_DISCO_BOOLEAN = 2
-GENDER_SALSA_BOOLEAN = 3
+GENDER_LATINO_BOOLEAN = 3
+GENDER_TECHNO_BOOLEAN = 4
+GENDER_EDM_BOOLEAN = 5
+GENDER_RETRO_BOOLEAN = 6
+
 
 BOOLEAN_PARAM = [('Genders', (
                         (GENDER_DISCO_BOOLEAN, 'Disco'),
-                        (GENDER_SALSA_BOOLEAN, 'Salsa'),
-                            )),
+                        (GENDER_LATINO_BOOLEAN, 'Latino'),
+                        (GENDER_TECHNO_BOOLEAN, 'Techno'),
+                        (GENDER_EDM_BOOLEAN, 'EDM'),
+                        (GENDER_RETRO_BOOLEAN, 'Retro'),
+)),
                  ('Others', (
                         (VOICES_BOOLEAN, 'Voices'),
                             ))
