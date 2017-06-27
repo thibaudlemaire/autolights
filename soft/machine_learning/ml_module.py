347ba195e31008c1b6f3d871f6c6d2e238a8f387
Raspi <tlemaire@enst.fr>
2017-06-27 12:53:58 +0200

    add manager

--- soft/machine_learning/ml_module.py
+++ soft/machine_learning/ml_module.py
@@ -5,22 +5,24 @@
 """
 import logging
 import queue
+import time
 from threading import Thread
 
 
 # This class provide a thread for the ML module
 class MlModule(Thread):
-    def __init__(self):
+    def __init__(self, manager):
         Thread.__init__(self)
         self.terminated = False  # Stop flag
         self.audio_queue = queue.Queue() # Audio frames FIFO
+        self.manager = manager
 
     # Thread processing ML
     def run(self):
         logging.info("Starting Machine Learning thread")
         # This loop condition have to be checked frequently, so the code inside may not be blocking
         while not self.terminated:
-            None
+            time.sleep(1)
             # Write here non-blocking code (use timeout...)
 
     # Method called to stop the thread
