3d77d1717ba96abfa5e91f6da4672c7bc04102fe
Raspi <tlemaire@enst.fr>
2017-06-27 18:02:00 +0200

    ML module

--- soft/machine_learning/ml_module.py
+++ soft/machine_learning/ml_module.py
@@ -5,8 +5,17 @@
 """
 import logging
 import queue
-import time
+import librosa
+import numpy as np
+from sklearn.externals import joblib
 from threading import Thread
+
+# Constants
+BUFFER_SIZE = 400           # Number of frames to store in the buffer (200 -> 5s)
+SAMPLE_PER_FRAME = 1024     # See audio module
+SAMPLE_RATE = 44100         # See audio module
+MODEL_PATH = 'machine_learning/data/model.pkl'         # SVM Model
+MFCC_COUNT = 130            # Number of MFCC
 
 
 # This class provide a thread for the ML module
@@ -14,21 +23,39 @@ class MlModule(Thread):
     def __init__(self, manager):
         Thread.__init__(self)
         self.terminated = False  # Stop flag
-        self.audio_queue = queue.Queue() # Audio frames FIFO
+        self.queue = queue.Queue()  # Contain 5ms frames
+        self.current_gender = None
+        self.counter = 0
+        self.frames = None
         self.manager = manager
+        self.svm = joblib.load(MODEL_PATH)
 
     # Thread processing ML
     def run(self):
-        logging.info("Starting Machine Learning thread")
+        logging.info("Starting Gender detector")
         # This loop condition have to be checked frequently, so the code inside may not be blocking
         while not self.terminated:
-            time.sleep(1)
-            # Write here non-blocking code (use timeout...)
+            new_frame = self.queue.get()  # Get new frame (blocking)
+            if self.counter == 0:
+                self.frames = new_frame
+                self.counter += 1
+            elif self.counter >= BUFFER_SIZE:
+                self.frames = np.append(self.frames, new_frame)
+                mfcc = librosa.feature.mfcc(self.frames, SAMPLE_RATE, n_mfcc=MFCC_COUNT)
+                result = self.svm.predict(mfcc)
+                if result[0] != self.current_gender:
+                    self.current_gender = result[0]
+                    self.manager.new_gender(result[0])
+                self.counter = 0
+            else:
+                self.frames = np.append(self.frames, new_frame)
+                self.counter += 1
 
     # Method called to stop the thread
     def stop(self):
         self.terminated = True
+        self.audio_frames.put(np.empty(SAMPLE_PER_FRAME, dtype=np.int16))  # Release blocking getter
 
     # Method called by the audio module when new audio frames are available
     def new_audio(self, audio_frames):
-        self.audio_queue.put(audio_frames)  # Put new frames in the FIFO
+        self.queue.put(audio_frames)  # Put new frames in the FIFO
